37cdd201006170dc442d9eece8550720198854e5
Ashish Kumar <ashish.kumar11@nagarro.com>
2020-03-31 15:17:11 +0530

    Integrate create product form

--- src/app/create-product/create-product.component.ts
+++ src/app/create-product/create-product.component.ts
@@ -7,62 +7,117 @@ import { FormControl, FormBuilder, FormGroup, Validators, FormControlName, FormA
   styleUrls: ['./create-product.component.css']
 })
 export class CreateProductComponent implements OnInit {
-    public addProductForm: FormGroup;
-    msgs: any[];
-    
-    @Output() onSubmit = new EventEmitter<any>();
+  public addProductForm: FormGroup;
+  msgs: any[];
 
-    constructor(private formBuilder: FormBuilder) {
-        this.createProductForm();
-    }
-    ngOnInit() {
-    }
+  @Output() onSubmit = new EventEmitter<any>();
 
-    createProductForm(name: any = '',
-        contact_person_name: any = '',
-        pricing: any = '') {
-        this.addProductForm = this.formBuilder.group({
-            name: [name,
-                {
-                    validators: Validators.compose([
-                        Validators.required,
-                        Validators.minLength(3),
-                        Validators.maxLength(30),
-                        Validators.pattern(/^[a-zA-Z0-9_\s]*$/)
-                    ]),
-                    updateOn: 'blur'
-                }
-            ],
-            contact_person_name: [contact_person_name,
-                {
-                    validators: Validators.compose([
-                        Validators.required,
-                        Validators.minLength(3),
-                        Validators.maxLength(30),
-                        Validators.pattern(/^[a-zA-Z0-9_\s]*$/)
-                    ]),
-                    updateOn: 'blur'
-                }
-            ],
-            venue_pricing: new FormArray([])
-        });
-        let formArrayInstance: any = this.addProductForm.get('venue_pricing');
-    }
+  constructor(private formBuilder: FormBuilder) {
+    this.createProductForm();
+  }
+  ngOnInit() {
+  }
 
-    // formArrayInstance.push(this.createVenuePricingForm()) 
-    createVenuePricingForm(para1, para2): FormGroup {
-        return this.formBuilder.group({
-            data1: para1,
-            data2: para2
-        });
-    }
-    locationSelection(data) {
-      console.log("called-->", data);
-    }
+  createProductForm(name: any = '',
+    description: any = '',
+    price: any = '',
+    category: any = '',
+    url: any = '',
+    number: any = '',
+    phoneType: any = '') {
+    this.addProductForm = this.formBuilder.group({
+      name: [name,
+        {
+          validators: Validators.compose([
+            Validators.required,
+            Validators.minLength(3),
+            Validators.maxLength(30),
+            // Validators.pattern(/^[a-zA-Z0-9_\s]*$/)
+          ]),
+          updateOn: 'blur'
+        }
+      ],
+      description: [description,
+        {
+          validators: Validators.compose([
+            Validators.required,
+            Validators.minLength(3),
+            Validators.maxLength(30),
+            Validators.pattern(/^[a-zA-Z0-9_\s]*$/)
+          ]),
+          updateOn: 'blur'
+        }
+      ],
+      price: [price,
+        {
+          validators: Validators.compose([
+            Validators.required,
+            Validators.minLength(3),
+            Validators.maxLength(30),
+            Validators.pattern(/^[a-zA-Z0-9_\s]*$/)
+          ]),
+          updateOn: 'blur'
+        }
+      ],
+      category: [category,
+        {
+          validators: Validators.compose([
+            Validators.required,
+            Validators.minLength(3),
+            Validators.maxLength(30),
+            Validators.pattern(/^[a-zA-Z0-9_\s]*$/)
+          ]),
+          updateOn: 'blur'
+        }
+      ],
+      url: [url,
+        {
+          validators: Validators.compose([
+            Validators.required,
+            Validators.minLength(3),
+            Validators.maxLength(30),
+            Validators.pattern(/^[a-zA-Z0-9_\s]*$/)
+          ]),
+          updateOn: 'blur'
+        }
+      ],
+      number: [number,
+        {
+          validators: Validators.compose([
+            Validators.required,
+            Validators.minLength(3),
+            Validators.maxLength(30),
+            Validators.pattern(/^[a-zA-Z0-9_\s]*$/)
+          ]),
+          updateOn: 'blur'
+        }
+      ],
+      phoneType: [phoneType,
+        {
+          validators: Validators.compose([
+            Validators.required,
+            Validators.minLength(3),
+            Validators.maxLength(30),
+            Validators.pattern(/^[a-zA-Z0-9_\s]*$/)
+          ]),
+          updateOn: 'blur'
+        }
+      ],
+      // formArray: new FormArray([])
+    });
+    let formArrayInstance: any = this.addProductForm.get('venue_pricing');
+  }
 
+  // formArrayInstance.push(this.createVenuePricingForm()) 
+  createVenuePricingForm(para1, para2): FormGroup {
+    return this.formBuilder.group({
+      data1: para1,
+      data2: para2
+    });
+  }
 
-    submit(data) {
-        console.log("submit called-->", data); 
-    }
+  submit(data) {
+    console.log("submit called-->", data);
+  }
 
 }

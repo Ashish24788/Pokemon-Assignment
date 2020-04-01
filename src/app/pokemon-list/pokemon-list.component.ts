f0f769b70a1a22c0c97d413f9e40f9212cb4b531
love9684 <lovelesh.kumar@nagarro.com>
2020-04-01 12:26:56 +0530

    update pokemon list

--- src/app/pokemon-list/pokemon-list.component.ts
+++ src/app/pokemon-list/pokemon-list.component.ts
@@ -1,55 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../core/user.service';
-import { Router } from '@angular/router';
 @Component({
   selector: 'app-pokemon-list',
   templateUrl: './pokemon-list.component.html',
   styleUrls: ['./pokemon-list.component.scss']
 })
 export class PokemonListComponent implements OnInit {
-  limit = 30;
-  totalCount = 0;
-  totalPage = 1;
-  currentPage = 1;
-  url = 'https://pokeapi.co/api/v2/pokemon/?limit=' + this.limit + '&offset=';
-  listData: {
-    name: string;
-    url: string
-  }[] = [];
-  jsonData: any = {};
+  url = 'https://pokeapi.co/api/v2/pokemon/?limit=30&offset=0';
+  respose: {
+    count: number;
+    next: string;
+    previous: string;
+    results: {
+      name: string;
+      url: string
+    }[]
+  };
 
-  constructor(private userService: UserService, private router: Router) {
-   }
+  constructor(private userService: UserService) {}
 
   ngOnInit() {
-    this.getPokemonData();
+    this.getPokemonData(this.url);
   }
 
-  getPokemonData() {
-    if (this.jsonData[this.currentPage]) {
-      this.listData = this.jsonData[this.currentPage];
-      return;
-    }
-    this.userService.get(this.url + (this.currentPage - 1)).subscribe(
-      res => {
-        this.totalCount = res.count;
-        this.totalPage = Math.ceil(this.totalCount / this.limit);
-        this.jsonData[this.currentPage] = res.results;
-        this.listData = res.results;
-      },
+  getPokemonData(url) {
+    this.userService.get(url).subscribe(
+      res => this.respose = res,
       err => console.log('HTTP Error', err),
       () => console.log('HTTP request completed.')
     );
   }
-
-  back() {
-    --this.currentPage;
-    this.getPokemonData();
-  }
-
-  next() {
-    ++this.currentPage;
-    this.getPokemonData();
-  }
-
 }

acb0f43898a3b2accaa4f918af4b5b5aad56a9b9
Komal Gupta <komgupta1@publicisgroupe.net>
2020-04-01 13:42:32 +0530

    work on create product form

--- src/app/create-product/create-product.component.ts
+++ src/app/create-product/create-product.component.ts
@@ -12,10 +12,7 @@ export class CreateProductComponent implements OnInit {
   reg = '(https?://)?([\\da-z.-]+)\\.([a-z.]{2,6})[/\\w .-]*/?';
   public productList: FormArray;
 
- // @Output() onSubmit = new EventEmitter<any>();
-
   constructor(private fb: FormBuilder) {
-  //  this.createProductForm();
   }
 
   get contactFormGroup() {
@@ -26,15 +23,19 @@ export class CreateProductComponent implements OnInit {
     return this.fb.group({
       name: [null, Validators.compose([Validators.required, Validators.minLength(3), Validators.pattern(/^[a-zA-Z0-9_\s]*$/)])],
       description: [null, Validators.compose([Validators.required, Validators.minLength(3), Validators.pattern(/^[a-zA-Z0-9_\s]*$/)])],
-      price: [null, Validators.compose([Validators.required, Validators.minLength(1), Validators.maxLength(2)])],
+      price: [null, Validators.compose([Validators.required, Validators.pattern(/^\d+\.\d{2}$/)])],
       category: ['electronics', Validators.compose([Validators.required])],
       imageURL: [null, Validators.compose([Validators.required, Validators.pattern(this.reg)])],
-      phone: [null, Validators.compose([Validators.required, Validators.maxLength(10)])],
+      phone: [null, Validators.compose([Validators.required, Validators.maxLength(10), Validators.pattern(/^\D?(\d{3})\D?\D?(\d{3})\D?(\d{4})$/)])],
       select: ['mobile']
     });
   }
 
   ngOnInit() {
+    this.productForm();
+  }
+
+  productForm() {
     this.addProductForm = this.fb.group({
       form: this.fb.array([this.createProduct()])
     });
@@ -47,120 +48,15 @@ export class CreateProductComponent implements OnInit {
     } else {
       alert('you can add max 5 product');
     }
-    console.log('productList', this.productList.length);
-  }
-
-  // remove contact from group
-  removeContact(index) {
-    this.productList.removeAt(index);
-  }
-
-  changedContactType(index) {
-    console.log('index', index);
   }
 
   onFormSubmit() {
-    console.log('submit');
+    this.addProductForm && this.addProductForm.value &&
+     localStorage.setItem('addProductForm', JSON.stringify(this.addProductForm.value.form));
+     this.productForm();
   }
 
   resetForm() {
-    console.log('reset');
-  }
-
-  // createProductForm(name: any = '',
-  //   description: any = '',
-  //   price: any = '',
-  //   category: any = '',
-  //   url: any = '',
-  //   number: any = '',
-  //   phoneType: any = '') {
-  //   this.addProductForm = this.formBuilder.group({
-  //     name: [name,
-  //       {
-  //         validators: Validators.compose([
-  //           Validators.required,
-  //           Validators.minLength(8),
-  //           Validators.pattern(/^[a-zA-Z0-9_\s]*$/)
-  //         ]),
-  //         updateOn: 'blur'
-  //       }
-  //     ],
-  //     description: [description,
-  //       {
-  //         validators: Validators.compose([
-  //           Validators.required,
-  //           Validators.minLength(3),
-  //           Validators.pattern(/^[a-zA-Z0-9_\s]*$/)
-  //         ]),
-  //         updateOn: 'blur'
-  //       }
-  //     ],
-  //     price: [price,
-  //       {
-  //         validators: Validators.compose([
-  //           Validators.required,
-  //           Validators.pattern(/^\d+\.\d{2}$/)
-  //         ]),
-  //         updateOn: 'blur'
-  //       }
-  //     ],
-  //     category: [category,
-  //       {
-  //         validators: Validators.compose([
-  //           Validators.required
-  //         ]),
-  //         updateOn: 'blur'
-  //       }
-  //     ],
-  //     url: [url,
-  //       {
-  //         validators: Validators.compose([
-  //           Validators.required,
-  //           Validators.pattern(this.reg)
-  //         ]),
-  //         updateOn: 'blur'
-  //       }
-  //     ],
-  //     number: [number,
-  //       {
-  //         validators: Validators.compose([
-  //           Validators.required,
-  //           Validators.maxLength(10),
-  //           Validators.pattern("^[0-9]*$"),
-  //         ]),
-  //         updateOn: 'blur'
-  //       }
-  //     ],
-  //     phoneType: [phoneType,
-  //       {
-  //         validators: Validators.compose([
-  //           Validators.required
-  //         ]),
-  //         updateOn: 'blur'
-  //       }
-  //     ],
-  //     // formArray: new FormArray([])
-  //   });
-  //   let formArrayInstance: any = this.addProductForm.get('venue_pricing');
-  // }
-
-  // formArrayInstance.push(this.createVenuePricingForm()) 
-  // createVenuePricingForm(para1, para2): FormGroup {
-  //   return this.formBuilder.group({
-  //     data1: para1,
-  //     data2: para2
-  //   });
-  // }
-
-  submit() {
-    if (this.addProductForm.valid) {
-      const list:any = [this.addProductForm.value];
-      localStorage.setItem('addProductForm', JSON.stringify(list));
-    }
-    this.addProductForm.reset();
-  }
-
-  reset() {
-    this.addProductForm.reset();
+    this.productForm();
   }
 }

1821ee28975e6d4e98996f24bcd003ab56c28e4a
love9684 <lovelesh.kumar@nagarro.com>
2020-04-02 22:09:26 +0530

    update pokemon detail

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -1,18 +1,31 @@
 import { Component, OnInit } from '@angular/core';
-import { UserService } from "../core/user.service";
+import { UserService } from '../core/user.service';
 
 @Component({
   selector: 'app-product-detail',
   templateUrl: './product-detail.component.html',
-  styleUrls: ['./product-detail.component.css']
+  styleUrls: ['./product-detail.component.scss']
 })
 export class ProductDetailComponent implements OnInit {
-  detailData: any;
+  response: any = {};
+  flavorObject: any = {};
+  generaObject: any = {};
 
   constructor(private userService: UserService) {}
 
   ngOnInit(): void {
-    this.detailData = this.userService.detailData;
+    this.response = this.userService.detailData || {};
+    if (this.response.species && this.response.species.url) {
+      this.getSpeciesData();
+    }
+  }
+
+  getSpeciesData() {
+    this.userService.get(this.response.species.url).subscribe(res => {
+      // this.speciesData = res;
+      this.flavorObject = res.flavor_text_entries.find(ob => ob.language.name === 'en') || {};
+      this.generaObject = res.genera.find(ob => ob.language.name === 'en') || {};
+    });
   }
 
 }

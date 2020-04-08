49082cf1dd132ffcffa0358b3a890e4dc411b354
Ashish Kumar <ashish.kumar11@nagarro.com>
2020-04-08 22:17:34 +0530

    Alert Notification done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { UserService } from './core/user.service';
 import { delay } from 'rxjs/operators';
+import { AlertService } from './core/alert.service';
+import { SYSTEM_CONSTANTS } from './core/system.constants';
 
 @Component({
   selector: 'app-root',
@@ -10,12 +12,27 @@ import { delay } from 'rxjs/operators';
 export class AppComponent {
   title = 'angular-assignment';
   showLoaderImage = false;
+  showAlertFlag = false;
+  alertMessage: any = {};
+  @ViewChild('alert', { static: true }) alert: ElementRef;
+  constructor(private userService: UserService, private alertService: AlertService) {
+    this.alertService.getAlert().pipe(
+      delay(SYSTEM_CONSTANTS.DELAY_TIME)
+    ).subscribe(data => {
+      this.alertMessage = data;
+      setTimeout(() => {
+        this.closeAlert();
+      }, SYSTEM_CONSTANTS.ALERT_FADE_OUT_TIME);
 
-  constructor(private userService: UserService) {
+    });
     this.userService
       .getLoader().pipe(
-        delay(0)
+        delay(SYSTEM_CONSTANTS.DELAY_TIME)
       )
       .subscribe((res) => (this.showLoaderImage = res));
+  }
+
+  closeAlert() {
+      // this.alert.nativeElement.classList.remove(SYSTEM_CONSTANTS.FADE_IN_CLASS);
   }
 }
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,12 +2,13 @@ import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { UserService } from './user.service';
+import { AlertService } from './alert.service';
 
 @NgModule({
   imports: [CommonModule, FormsModule],
   declarations: [],
   exports: [],
-  providers: [UserService],
+  providers: [UserService, AlertService],
 })
 export class CoreModule {
   constructor(@Optional() @SkipSelf() parentModule: CoreModule) {

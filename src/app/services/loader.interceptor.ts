e06b923391be5e259c5ac01fe0ef6c241386f5a2
Ashish Kumar <ashish.kumar11@nagarro.com>
2020-04-09 11:05:38 +0530

    fixed: alert Notification catch: interceptor

--- src/app/services/loader.interceptor.ts
+++ src/app/services/loader.interceptor.ts
@@ -1,15 +1,29 @@
 import { Injectable } from "@angular/core";
-import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
-import { Observable } from "rxjs";
-import { finalize } from "rxjs/operators";
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HttpErrorResponse, HttpResponse } from "@angular/common/http";
+import { Observable, of, throwError } from "rxjs";
+import { finalize, catchError } from "rxjs/operators";
 import { UserService } from '../core/user.service';
+import { AlertService } from '../core/alert.service';
+import { VALIDATION_MSG, SYSTEM_CONSTANTS } from '../core/system.constants';
 @Injectable()
 export class LoaderInterceptor implements HttpInterceptor {
-    constructor(public userService: UserService) { }
+    constructor(public userService: UserService, private alertService:AlertService) { }
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         this.userService.showLoader(true);
         return next.handle(req).pipe(
-            finalize(() => this.userService.showLoader(false))
-        );
+            finalize(() => this.userService.showLoader(false)),
+            catchError((err: any) => {
+                if (err instanceof HttpErrorResponse) {
+                    try {
+                        this.alertService.showAlert({ text: err.error, class: SYSTEM_CONSTANTS.ERROR_CLASS });
+                    } catch (e) {
+                        this.alertService.showAlert({ text: err.error, class: SYSTEM_CONSTANTS.ERROR_CLASS });
+                    }
+                    //log error 
+                }
+                return of(err);
+                // return throwError(err.error);
+            }));
+
     }
 }
